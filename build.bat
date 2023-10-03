e9d3bf0267d652306b8ae422e922ee84975e1154
Hasane <hasan.ibishi@hotmail.com>
2023-10-03 19:47:30 +0200

    batch file updated

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-call ng build --prod --base-href "https://hasanibishi.github.io/signal-standalone/"
+call ng build --configuration production --base-href "https://hasanibishi.github.io/signal-standalone/"
 call ngh --dir dist/signal-standalone
 echo signal-standalone built
